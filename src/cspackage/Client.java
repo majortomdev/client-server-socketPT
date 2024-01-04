37a0a0dce724138f5df73d76837032063f914fcf
Joseph Kinahan <kinahanj25@gmail.com>
2024-01-04 22:21:55 +0000

    put in readers and writers

--- src/cspackage/Client.java
+++ src/cspackage/Client.java
@@ -1,6 +1,9 @@
 package cspackage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -8,8 +11,13 @@ public class Client {
 
 	public static void main(String[] args) {
 		try {
-			System.out.println("Client Started");
 			Socket soc = new Socket("localhost", 8888);
+			
+			BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+			
+			PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/cspackage/Server.java
+++ src/cspackage/Server.java
@@ -1,6 +1,9 @@
 package cspackage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,10 +11,12 @@ public class Server {
 
 	public static void main(String[] args) {
 		try {
-			System.out.println("Waiting for client....");
 			ServerSocket ss = new ServerSocket(8888);
 			Socket soc = ss.accept();
-			System.out.println("Connection Established");
+			BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+			
+			PrintWriter out = new PrintWriter(soc.getOutputStream(),true);//boolean sets to auto flush it,
+			//not keep it in buffer until i manual flush it
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
