4cf0ed9a9ce09f66bc304e9c142021110a5c3654
Joseph Kinahan <kinahanj25@gmail.com>
2024-01-05 01:10:44 +0000

    built simple i/o calculator using socket/readers/writers

--- src/cspackage/Client.java
+++ src/cspackage/Client.java
@@ -18,6 +18,34 @@ public class Client {
 			BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 			
 			PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+			
+			int option = 0;
+			int num1= 0;
+			int num2= 0;
+			do {
+				System.out.println("Choose an operation");
+				System.out.println("1. Addition");
+				System.out.println("2. Subtraction");
+				System.out.println("3. Multiplication");
+				System.out.println("4. Division");
+				System.out.println("Enter an option");
+				option = Integer.parseInt(userInput.readLine());
+				
+				System.out.println("Enter first number");
+				num1 = Integer.parseInt(userInput.readLine());
+				
+				System.out.println("Enter second number");
+				num2 = Integer.parseInt(userInput.readLine());
+				
+				out.println(option+":"+num1+":"+num2);//im using the printwriter to send data to server as 
+				//....  a string with the semi colon delimiter
+				
+				String answer = in.readLine();
+				System.out.println("Server responds with:  "+answer);
+				System.out.println();
+				
+			}while(true);
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/cspackage/Server.java
+++ src/cspackage/Server.java
@@ -12,11 +12,40 @@ public class Server {
 	public static void main(String[] args) {
 		try {
 			ServerSocket ss = new ServerSocket(8888);
+			System.out.println("Waiting for client...");
 			Socket soc = ss.accept();
+			System.out.println("Established Connection");
 			BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 			
 			PrintWriter out = new PrintWriter(soc.getOutputStream(),true);//boolean sets to auto flush it,
 			//not keep it in buffer until i manual flush it
+			
+			while(true) {
+				String strChoices[] = in.readLine().split(":");
+				
+				int option = Integer.parseInt(strChoices[0]);
+				int num1 = Integer.parseInt(strChoices[1]);
+				int num2 = Integer.parseInt(strChoices[2]);
+				String result = "";
+				
+				switch(option) {
+				case 1: result = "Addition is: "+(num1+num2);break;
+				case 2: result = "Subtraction is: "+(num1-num2);break;
+				case 3: result = "Multiplication is: "+(num1*num2);break;
+				case 4: result = "Division is: "+(num1/num2);break;
+				default:break;
+				}
+				
+				//System.out.println(result);
+				out.println(result);
+			}
+			
+			
+			
+			
+			
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
